1206c192be011618559f9b44766f96175cf1f166
Xiao Wang <wangxiao@gmail.com>
2016-11-02 12:04:26 -0400

    remove doc

--- html/navtreedata.js
+++ html/navtreedata.js
@@ -1,7 +1,6 @@
 var NAVTREE =
 [
   [ "emp-toolkit", "index.html", [
-    [ "em-doc", "md__r_e_a_d_m_e.html", null ],
     [ "emp-input-validity", "md___users_wangxiao_git_emp-toolkit_emp-input-validity__r_e_a_d_m_e.html", null ],
     [ "emp-ot", "md___users_wangxiao_git_emp-toolkit_emp-ot__r_e_a_d_m_e.html", null ],
     [ "emp-tool", "md___users_wangxiao_git_emp-toolkit_emp-readme__r_e_a_d_m_e.html", null ],
@@ -40,7 +39,7 @@ var NAVTREEINDEX =
 "class_o_t_iterated.html#a76d9f6ff16d829b0387a95de4b7063c2",
 "emp-ot_2_c_make_files_23_82_82_2_compiler_id_c_2_c_make_c_compiler_id_8c.html#a4b0efeb7a5d59313986b3a0390f050f6",
 "garble_2block_8h.html#a895bc02edcaa0a0798728f538af35eb5",
-"structgarble__fixed__wire.html"
+"structgarble__fixed__wire.html#a61f61fae6823013387ee3f7d08780af0"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
--- html/search/pages_0.js
+++ html/search/pages_0.js
@@ -4,6 +4,5 @@ var searchData=
   ['emp_2dot',['emp-ot',['../md___users_wangxiao_git_emp-toolkit_emp-ot__r_e_a_d_m_e.html',1,'']]],
   ['emp_2dtool',['emp-tool',['../md___users_wangxiao_git_emp-toolkit_emp-readme__r_e_a_d_m_e.html',1,'']]],
   ['emp_2dsh2pc',['emp-sh2pc',['../md___users_wangxiao_git_emp-toolkit_emp-sh2pc__r_e_a_d_m_e.html',1,'']]],
-  ['emp_2dtool',['emp-tool',['../md___users_wangxiao_git_emp-toolkit_emp-tool__r_e_a_d_m_e.html',1,'']]],
-  ['em_2ddoc',['em-doc',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['emp_2dtool',['emp-tool',['../md___users_wangxiao_git_emp-toolkit_emp-tool__r_e_a_d_m_e.html',1,'']]]
 ];
